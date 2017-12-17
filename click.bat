de7ef8de9f001c5a4a994dbd9b2d86abf49fdcca
DevHyung <khu[hj@gmail.com>
2017-12-17 16:10:03 +0900

    :memo: copy to github in my desktop file

---
+++ click.bat
@@ -0,0 +1,3 @@
+@echo off
+set /p pic="picture? "
+start vlens_semiauto %pic%.jpg data/lens/007.png result.png
---
+++ input.bat
@@ -0,0 +1,3 @@
+@echo off
+set /p pic="picture? "
+start vlens_semiauto %pic%.jpg data/lens/007.png result.png 347,572 393,580 357,604 383,604 639,562 593,573 629,591 609,596
